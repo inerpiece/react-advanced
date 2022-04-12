d2da322dfa11325c5207865f4168325a38f642b5
inerpiece <anton.p.markov@gmail.com>
2022-04-12 13:53:37 +0200

    Installed react-router-dom and utilized the Routes and Route components to create a working routing system, next up is adding a navigation for easier access to the routes.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups'
+import FavoritesPage from './pages/Favorites'
+import NewMeetupPage from './pages/NewMeetup'
 
 function App() {
   return (
     <div>
-      Hi
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
     </div>
   );
 }
